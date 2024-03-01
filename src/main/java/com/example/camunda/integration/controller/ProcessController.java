4c34cb4753729187eddbd5b73d86bb4ce457bd2d
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-03-01 23:18:58 +0200

    add hybernate and annotations to request pojo

--- src/main/java/com/example/camunda/integration/controller/ProcessController.java
+++ src/main/java/com/example/camunda/integration/controller/ProcessController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@RestController("/process")
+@RestController
+@RequestMapping("/process")
 public class ProcessController {
 
     @Autowired
--- src/main/java/com/example/camunda/integration/dtos/ProcessRequest.java
+++ src/main/java/com/example/camunda/integration/dtos/ProcessRequest.java
@@ -1,10 +1,17 @@
 package com.example.camunda.integration.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
+@Getter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ProcessRequest {
+    
     private String name;
 }

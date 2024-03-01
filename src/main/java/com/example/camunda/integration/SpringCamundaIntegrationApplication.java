c72f913bb3371a9d0ec811486178b343997c98e0
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-03-01 21:25:34 +0200

    first commit

---
+++ src/main/java/com/example/camunda/integration/SpringCamundaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamundaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamundaIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/camunda/integration/SpringCamundaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camunda.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamundaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

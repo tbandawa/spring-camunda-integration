10642202f6cd355efff1b62ee923ca79da1eaa20
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-03-01 22:23:48 +0200

    add delegate classes

---
+++ src/main/java/com/example/camunda/integration/tasks/StageOneDelegate.java
@@ -0,0 +1,17 @@
+package com.example.camunda.integration.tasks;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class StageOneDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution arg0) throws Exception {
+        log.info("---------------> executing stage one");
+    }
+}
